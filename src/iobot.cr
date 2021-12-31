5fde1f6ba60ea08b9a9eec76bbcc6af0e48e60e7
Eric Will <rakaur@malkier.net>
2021-12-30 20:49:45 -0600

    Toss custom config in favor of totem.

--- src/iobot.cr
+++ src/iobot.cr
@@ -1,31 +1,52 @@
 # TODO: Write documentation for `Iobot`
 require "clim"
-require "yaml"
+require "totem"
 
-require "./configuration"
+require "iolite"
 
 module Iobot
   VERSION = "0.1.0"
 
-  DESCRIPTION = "iobot - a simple discord bot using iolite"
+  DESCRIPTION = "iobot - a simple irc bot using iolite"
 
   class CLI < Clim
     main do
       desc DESCRIPTION
       usage "iobot [options]"
-      help short: "-h"
       version "iobot: version iobot-#{Iobot::VERSION}", short: "-v"
+
+      help short: "-h"
+
+      option "-c FILE", "--config=FILE", type: String,
+        desc: "Specify configuration file",
+        default: "config.yml"
 
       run do |opts, args|
         puts version
         puts "#{args.all_args.join(", ")}!"
 
+        # Configuration
         begin
-          Configuration.new.parse
-        rescue e : Configuration::ParseError
-          puts "iobot: configuration error"
-          raise e
+          config = Totem.from_file opts.config
+        rescue Totem::Exception::NotFoundConfigFileError
+          puts %(iobot: configuration file "#{opts.config}" not found)
+          exit
+        rescue exception
+          puts "iobot: unhandled error in configuration"
+          raise exception
         end
+
+        client = Iolite::Client.new(
+          server: config["uplink.server"].as_s?,
+          port: config["uplink.port"].as_i?,
+          password: config["uplink.password"].as_s?,
+
+          nickname: config["irc.nickname"].as_s?,
+          username: config["irc.username"].as_s?,
+          realname: config["irc.realname"].as_s?
+        )
+
+        p client
       end
     end
   end
